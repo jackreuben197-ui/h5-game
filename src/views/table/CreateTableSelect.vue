194b5fcf9a26468c183c0d7598dc9aa2f2ff1cef
wangjie <soolaryo@gmail.com>
2026-05-14 11:00:36 +0800

    fix: 关闭mtt创建入口

--- src/views/table/CreateTableSelect.vue
+++ src/views/table/CreateTableSelect.vue
@@ -33,7 +33,7 @@ const gameTypes: GameTypeItem[] = [
   { key: 'nlh', game_play_type: 1, title: '德州', icon: iconNlh },
   { key: 'plo', game_play_type: 2, title: '奥马哈', icon: iconPlo },
   { key: 'six_plus', game_play_type: 3, title: '6+', icon: iconSixPlus },
-  { key: 'mtt', game_play_type: 7, title: 'MTT', icon: iconMTT },
+  // { key: 'mtt', game_play_type: 7, title: 'MTT', icon: iconMTT },
   // { key: 'aof', title: 'AOF', icon: iconAof },
   // { key: 'mushroom', title: '蘑菇桌', icon: iconMushroom },
   // { key: 'squid', title: '深海桌', icon: iconSquid },
